e0d61faf21aa17ee6f99d12fdb3b9398645b38b3
rajasatyam <satyamraja005@gmail.com>
2025-11-01 00:07:33 +0530

    fix typos and add comment api

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose"
 import {Comment} from "../models/comment.model.js"
+import {Video} from "../models/videos.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
@@ -13,6 +14,27 @@ const getVideoComments = asyncHandler(async (req, res) => {
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    const {videoId} = req.params
+    const {content} = req.body
+    if(!content){
+        throw new ApiError(400 , "content is required")
+    }
+    const video = await Video.findById(videoId);
+    if(!video){
+        throw new ApiError(404 , "video does not exist")
+    }
+
+    const comment = await Comment.create({
+        content , 
+        video: videoId,
+        owner : req.user?._id 
+    })
+
+    if(!comment){
+        throw new ApiError(500 , "failed to add comment , please try again")
+    }
+
+    return res.status(201).json(new ApiResponse(200 , comment , "comment added successfully"))
 })
 
 const updateComment = asyncHandler(async (req, res) => {
--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -22,4 +22,4 @@ const commentSchema = new Schema(
 
 commentSchema.plugin(mongooseAggregatePaginate)
 
-export const Comments = mongoose.model("Comments" , commentSchema)
+export const Comment = mongoose.model("Comment" , commentSchema)
