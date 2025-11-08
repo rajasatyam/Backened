8f23d6659a34eff3560e3b707f84070decf85c4a
rajasatyam <satyamraja005@gmail.com>
2025-11-08 09:44:18 +0530

    Implement updateComment functionality with validation and error handling

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -9,7 +9,6 @@ const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
-
 })
 
 const addComment = asyncHandler(async (req, res) => {
@@ -39,6 +38,36 @@ const addComment = asyncHandler(async (req, res) => {
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    const {videoId , commentId} = req.params
+    const {content} = req.body
+
+    const video = await Video.findById(videoId)
+    if(!video){
+        throw new ApiError(404 , "video does not exist")
+    }
+
+    const comment = await Comment.findById(commentId)
+    if(!comment){
+        throw new ApiError(404 , "comments not exist for updation")
+    }
+    if(!content){
+        throw new ApiError(400 , "content is required")
+    }
+    if(comment.owner.to_string() !== req.user?._id.to_string()){
+        throw new ApiError(400 , "unauthorized request")
+    }
+
+    const updatedComment = await Comment.findByIdAndUpdate(
+        {
+            commentId,
+            content : content,
+            video: videoId,
+            owner : req.user?._id
+        }
+    )
+
+    return res.status(201).json(new ApiResponse(200 , comment , "comment added successfully"))
+    
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
